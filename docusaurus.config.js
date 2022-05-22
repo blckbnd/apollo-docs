5c1ca04bfde702f1f76833d36e23aa45c51eb961
Jonas Bostoen <jonas.bostoen@student.kdg.be>
2022-05-22 12:53:51 -0400

    fix: gh pages deploy: base-url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Apollo Docs',
   tagline: 'Documentation for Apollo',
   url: 'https://blckbnd.github.io/apollo-docs',
-  baseUrl: '/',
+  baseUrl: '/apollo-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
