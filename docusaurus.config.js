cc766d7f489ad0b37f2b2aab968e28e09e96042d
Jonas Bostoen <jonas.bostoen@hotmail.com>
2023-03-06 20:40:32 +0100

    fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Apollo Docs',
   tagline: 'Documentation for Apollo',
   url: 'https://apollo.chainbound.io',
-  baseUrl: '/',
+  baseUrl: '/apollo-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
